74bb8b629d352bfdf3bc0523992009931ca7515b
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2022-05-25 14:19:53 +0200

    Add gatsby-plugin-gatsby-cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {},
+        allPageHeaders: [],
+        mergeSecurityHeaders: true,
+        mergeLinkHeaders: true,
+        mergeCachingHeaders: true,
+        transformHeaders: (headers, path) => headers,
+        generateMatchPathRewrites: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
