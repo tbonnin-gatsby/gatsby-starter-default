4afe8906069de204400a26408e60184425d16339
Thomas Bonnin <233326+TBonnin@users.noreply.github.com>
2022-12-08 10:16:13 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,7 @@
 exports.createPages = async ({ actions }) => {
-  const { createPage, createRedirect } = actions
+  const { createRedirect } = actions
   createRedirect({
     fromPath: `/index.html`,
     toPath: `/`,
-  });
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
   });
 }
